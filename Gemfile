96b3d46cf1ebcd6e784396babe9a47b5f2aac42a
Drew Verlee <drew.verlee@atomicobject.com>
2014-06-06 15:56:06 -0400

    Create blank rails app.

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+ruby '2.1.2'
+#ruby-gemset=blackjack_4_0
+
+gem 'rails', '4.0.5'
+
+group :development do
+  gem 'pg'
+end
+
+group :test, :development do
+  gem 'fabrication', '2.11.2'
+  gem 'cucumber', '~> 1.3.15'
+  gem 'capybara-webkit'
+  gem 'capybara'
+  gem 'konacha', '~> 3.2.3'
+end
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blackjack::Application.config.secret_key_base = '863450d018b5f6128c0fba2b1f274a04c5def6e4b1ac00c4584f212b42e4f7ca79499733adb54f23c2369756b4214355824e4438d8f67725a06f9297d44bcff4'
