8f2c093cd9cbc31b3e2af2596f7031f1ef11841a
Drew Verlee <drew.verlee@atomicobject.com>
2014-06-12 09:45:02 -0400

    Passing - get lobby information

--- app/controllers/api/v1/lobby_controller.rb
+++ app/controllers/api/v1/lobby_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class LobbyController < ApplicationController
       def index
-        render json: [{"level" => 'Beginner'}], status: 200
+        render json: {"level" => 'Beginner', 'totalChips' => '0'}, status: 200
       end
     end
   end
