d807d76bbbeee3e97804ae0b50577d681d9990af
Drew Verlee <drew.verlee@atomicobject.com>
2014-06-16 13:32:36 -0400

    Made lobby story stronger.

--- features/step_definitions/visiting_lobby_steps.rb
+++ features/step_definitions/visiting_lobby_steps.rb
@@ -10,3 +10,9 @@ And /^a player stats widget.$/ do
   page.has_css?('.app-player-stats-widget')
 end
 
+And /^that player stats widget shows his total chip count.$/ do 
+  player = Player.first 
+  player.total_chips = 0
+  page.has_text?(player.total_chips)
+  page.has_text?(0)
+end
--- features/visit_lobby.feature
+++ features/visit_lobby.feature
@@ -4,4 +4,5 @@ Feature: Exploring the lobby
     Given a user visits the lobby
     Then they should see an option to create a beginner game
       And a player stats widget.
+      And that player stats widget shows his total chip count.
   
