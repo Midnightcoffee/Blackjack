1a118b1e1d5e1b8657feeec65479e68ba0f07d46
Drew Verlee <drew.verlee@atomicobject.com>
2014-06-18 14:30:36 -0400

    Actual passing - visit lobby feature

--- features/step_definitions/visiting_lobby_steps.rb
+++ features/step_definitions/visiting_lobby_steps.rb
@@ -2,17 +2,18 @@ Given /^a user visits the lobby$/ do
   visit '/'
 end
 
-# Then /^they should see an option to create a beginner game$/, js: true do
-#   expect(page).to have_text("Beginner")
-# end
+Then /^they should see an option to create a beginner game$/ do
+  expect(page).to have_text("Beginner")
+end
 
-# And /^a player stats widget.$/ do
-#   expect(page).to have_text("Total Chips:")
-# end
+And /^a player stats widget.$/ do
+  #TODO: should this be something else?
+  expect(page).to have_text("Total Chips:")
+end
 
-# And /^that player stats widget shows his total chip count.$/ do 
-#   player = Player.first 
-#   player.total_chips = 0
-#   expect(page).to have_text?(player.total_chips)
-#   page.has_text?(0)
-# end
+And /^that player stats widget shows his total chip count.$/ do 
+  player = Player.first 
+  player.total_chips = 0
+  expect(page).to have_text(player.total_chips)
+  expect(page).to have_text(0)
+end
