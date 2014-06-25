62029fed3a01c8559b9300aec1340091b6d801a1
Drew Verlee <drew.verlee@atomicobject.com>
2014-06-25 09:35:52 -0400

    Refactor levels factory into game factory.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,15 +2,15 @@
 
 Blackjack = angular.module("Blackjack", ["ngRoute", "ngResource"]);
 
-// FIXME: routeProviders could be chained?
+// FIXME: routeProviders should be chained?
 Blackjack.config(function ($routeProvider){
 
   $routeProvider.when("/", {
             templateUrl: "assets/main/lobby.html",
             controller: "LobbyCtrl",
             resolve: {
-                data: function (levelsResource) {
-                  return levelsResource.query();
+                games: function (gamesResource) {
+                  return gamesResource.getLevels();
               }
             }
   });
@@ -20,7 +20,7 @@ Blackjack.config(function ($routeProvider){
             controller: "GameCtrl",
             resolve: {
               data: function (gamesResource) {
-                return gamesResource.get();
+                return gamesResource.getCurrentGame();
               }
             }
   });
@@ -30,72 +30,63 @@ Blackjack.config(function ($routeProvider){
   });
 });
 
-Blackjack.controller("PlayerCtrl", function($scope, $http, playersResource){
+// --------------- Controllers ----------------------------
+Blackjack.controller("PlayerCtrl", function($scope, playersResource){
   $scope.players = playersResource.get();
 });
 
-//TODO: better nameing convention games vs levels.
-Blackjack.controller("LobbyCtrl", function($scope, $http, data){
-  //extra object "data" allows it to below to multiply controllers
-  $scope.games = data;
+Blackjack.controller("LobbyCtrl", function($scope, games){
+  $scope.games = games;
 });
 
-
 Blackjack.controller("GameCtrl", function($scope, $http, $location, $routeParams, 
-                                          data){
-
+                                          data, gamesResource){
   $scope.gameId = $routeParams.gameId;
   
   $scope.game = data;
+  $scope.game
 
   $scope.amount = 0;
-  // $scope.bet = function(){
-  //   data = {bet: $scope.amount}
-  //   // FIXME: hard coded player id, only one game currently
-  //   $http.put("/api/v1/players/1/games/1/bet", data)
-  //     .success(function () {
-  //       // hand code game id
+  //FIXME: the offical docs for ng-submit don't pass data as a argument,
+  //is this just an oversight? Why is it better to pass them as values.
+  $scope.bet = function(){
+    data = {bet: $scope.amount}
+    // FIXME: hard coded player id, only one game currently
+    
 
-  //       $http.get("/api/v1/players/1/games/1").success(function (data) {
-  //         $scope.game = data;
-  //       });
+    // $http.put("/api/v1/players/1/games/1/bet", data)
+    //   .success(function () {
+    //     // hand code game id
 
-  //     })
-  // };
+    //     $http.get("/api/v1/players/1/games/1").success(function (data) {
+    //       $scope.game = data;
+    //     });
+
+    //   })
+  };
 });
 
-// Blackjack.factory("gameFactory", function($http, $q) {
-
-//   return {
-
-//     data : {},
-//     getGameState : function(id){
-//       var urlBase = "/api/v1/players/1/games/";
-//       var defer = $q.defer();
-//       var data = this.data;
-//       $http.get(urlBase.concat(id)).success(function (data) {
-//         defer.resolve(data);
-//       });
-//       //TODO: on failure?
-//       return defer.promise;
-//     }
-//   };
-// });
-
 // --------------- FACTORIES ----------------------------
-//TODO: is it possible to have one factory for level and game?
 //
 Blackjack.factory("levelsResource", function($resource) {
   return $resource("/api/v1/games/levels");
 });
 
 Blackjack.factory("playersResource", function($resource) {
-  //FIXME: hardcoded 
+  //FIXME: hardcoded  player id
   return $resource("/api/v1/players/1");
 });
 
 Blackjack.factory("gamesResource", function($resource) {
-  //FIXME hardcoded
-  return $resource("/api/v1/players/1/games/1");
+  return {
+    getCurrentGame: function () {
+      return $resource("/api/v1/players/1/games/" + ":id").query(); 
+    
+    },
+
+    getLevels: function () {
+      return $resource("/api/v1/games/levels").query();  
+    }
+  }
 });
 
