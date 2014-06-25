afaf1d15c3d237830953e8846d60664a8973539e
Drew Verlee <drew.verlee@atomicobject.com>
2014-06-25 13:54:58 -0400

    Failing - feature - player can bet.

--- features/player_can_bet.feature
+++ features/player_can_bet.feature
@@ -1,10 +1,14 @@
 Feature: Player can bet
 
   Scenario: Successful bet
-    Given a user joins a game
+    Given the player and games exists
+      And a player joins a game
     When they bet
     Then their bet should be placed
-      And they should review cards
+      And they should see their cards
       And they should see the dealers card
+      And see the option to hold
+      And see the option to hit
+      And not see the option to bet
       #TODO: add more
   
--- features/step_definitions/player_can_bet_steps.rb
+++ features/step_definitions/player_can_bet_steps.rb
@@ -1,4 +1,34 @@
-Given /^a user joins a game$/ do
-  #achieve join game state 
-  visit '/game'
+Given(/^a player joins a game$/) do
+  visit '/#games/1'
 end
+
+When(/^they bet$/) do
+  fill_in "bet", :with => 20 
+  click_button("Bet")
+end
+
+Then(/^their bet should be placed$/) do
+  @game = Game.find(1)
+  expect(@game.player_bet).to equal(20) 
+end
+
+Then(/^they should see their cards$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^they should see the dealers card$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^see the option to hold$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^see the option to hit$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^not see the option to bet$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
