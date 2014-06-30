39d072f0fa7861716e81ce4d45a79eebbd64ec92
Drew Verlee <drew.verlee@atomicobject.com>
2014-06-30 12:30:39 -0400

    extend deck on game_factory

--- spec/factories/game_factory.rb
+++ spec/factories/game_factory.rb
@@ -5,6 +5,6 @@ FactoryGirl.define do
     player_bet 0
     player_hand ""
     dealer_hand ""
-    deck_sleeve "Diamond,Ace|Heart,Ace|Heart,9|Spade,Ace|Spade,10|"
+    deck_sleeve "Diamond,Ace|Heart,Ace|Heart,9|Spade,Ace|Spade,10|Heart,7|Spade,2|Spade,4|Spade,10|Heart,9|Heart,8|Heart,7|Heart,6|Heart,5|Club,10|Diamond,10|Diamond,5|Diamond,Jack|"
   end
 end
