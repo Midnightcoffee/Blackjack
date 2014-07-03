23cc585ea1ee772cb742ca6cc8b949dfb795f025
Drew Verlee <drew.verlee@atomicobject.com>
2014-07-03 10:53:02 -0400

    clean up db seeds for production.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 # FactoryGirl.create(:game, player_id: @player.id, level: "High Roller", min_bet: 100, max_bet: nil)
 
 @player = Player.create(id: 1, total_chips: 100)
-Game.create(:game, player_id: @player.id, level: "Beginner", min_bet: 1, max_bet: 50, deck_sleeve: "Heart,Ace|")
-Game.create(:game, player_id: @player.id, level: "Intermediate", min_bet: 50, max_bet: 100,deck_sleeve: "Heart,Ace|")
-Game.create(:game, player_id: @player.id, level: "High Roller", min_bet: 100, max_bet: nil,deck_sleeve: "Heart,Ace|")
+Game.create(player_id: @player.id, level: "Beginner", min_bet: 1, max_bet: 50, deck_sleeve: "Heart,Ace|")
+Game.create(player_id: @player.id, level: "Intermediate", min_bet: 50, max_bet: 100,deck_sleeve: "Heart,Ace|")
+Game.create(player_id: @player.id, level: "High Roller", min_bet: 100, max_bet: nil,deck_sleeve: "Heart,Ace|")
 
 
