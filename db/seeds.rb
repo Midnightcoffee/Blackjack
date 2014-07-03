7a1e52d7f9277232e2e4fb581aba4d3d5886aaec
Drew Verlee <drew.verlee@atomicobject.com>
2014-07-03 11:02:05 -0400

    add level to seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 # FactoryGirl.create(:game, player_id: @player.id, level: "Intermediate", min_bet: 50, max_bet: 100)
 # FactoryGirl.create(:game, player_id: @player.id, level: "High Roller", min_bet: 100, max_bet: nil)
 
-@player = Player.create(id: 1, total_chips: 100)
+@player = Player.create(id: 1, total_chips: 100, level: 1)
 Game.create(player_id: @player.id, level: "Beginner", min_bet: 1, max_bet: 50, deck_sleeve: "Heart,Ace|")
 Game.create(player_id: @player.id, level: "Intermediate", min_bet: 50, max_bet: 100,deck_sleeve: "Heart,Ace|")
 Game.create(player_id: @player.id, level: "High Roller", min_bet: 100, max_bet: nil,deck_sleeve: "Heart,Ace|")
