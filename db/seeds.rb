0b5e82f1e26c0925390b3e0eb880cf4a906a526e
Drew Verlee <drew.verlee@atomicobject.com>
2014-07-03 10:23:47 -0400

    Don't use factory girl for db seeds for production.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-require 'factory_girl_rails'
+#require 'factory_girl_rails'
 
 
-@player = FactoryGirl.create(:player, total_chips: 100, id: 1)
-FactoryGirl.create(:game, player_id: @player.id, level: "Beginner", min_bet: 1, max_bet: 50)
-FactoryGirl.create(:game, player_id: @player.id, level: "Intermediate", min_bet: 50, max_bet: 100)
-FactoryGirl.create(:game, player_id: @player.id, level: "High Roller", min_bet: 100, max_bet: nil)
+# @player = FactoryGirl.create(:player, total_chips: 100, id: 1)
+# FactoryGirl.create(:game, player_id: @player.id, level: "Beginner", min_bet: 1, max_bet: 50)
+# FactoryGirl.create(:game, player_id: @player.id, level: "Intermediate", min_bet: 50, max_bet: 100)
+# FactoryGirl.create(:game, player_id: @player.id, level: "High Roller", min_bet: 100, max_bet: nil)
+
+@player = Player.create(id: 1, total_chips: 100)
+Game.create(:game, player_id: @player.id, level: "Beginner", min_bet: 1, max_bet: 50, deck_sleeve: "Heart,Ace|")
+Game.create(:game, player_id: @player.id, level: "Intermediate", min_bet: 50, max_bet: 100,deck_sleeve: "Heart,Ace|")
+Game.create(:game, player_id: @player.id, level: "High Roller", min_bet: 100, max_bet: nil,deck_sleeve: "Heart,Ace|")
 
 
