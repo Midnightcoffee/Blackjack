c6085fa5919e7fadf27abebe076d79fc767dae0f
Drew Verlee <drew.verlee@atomicobject.com>
2014-07-03 10:02:43 -0400

    Turn off js uglifier.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Blackjack::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier.new(mange: false)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
